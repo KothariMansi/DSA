207e830e506c3c9b4e6f9607a36e3e3266769358
SHREE <mansikothari46@gmail.com>
2025-05-31 21:56:32 +0530

    Implement insertion, deletion and search in binary tree.

--- tree.c
+++ tree.c
@@ -8,11 +8,47 @@ typedef struct Node {
 
 } Node;
 
-Node* createNode(int val) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = val;
-    newNode->left = newNode->right = NULL;
-    return newNode;
+
+void insertNode(Node **tree, int val) {
+    Node* temp = NULL;
+    if (!(*tree))
+    {
+        Node* temp = (Node*)malloc(sizeof(Node*));
+        temp->left = temp->right = NULL;
+        temp->data = val;
+        *tree = temp;
+        return;
+    }
+    if (val < (*tree)->data) {
+        insertNode(&(*tree)->left, val);
+    } else if (val > (*tree)->data) {
+        insertNode(&(*tree)->right, val);
+    }
+}
+
+void deltree(Node *tree) {
+    if (tree)
+    {
+        deltree(tree->left);
+        deltree(tree->right);
+        free(tree);
+    }
+}
+
+Node* searchtree(Node **tree, int val) {
+    if (!(*tree))
+    {
+        return NULL;
+    }
+    if (val < (*tree)->data) {
+        searchtree(&(*tree)->left, val);
+    } else if (val > (*tree)->data)
+    {
+        searchtree(&(*tree)->right, val);
+    } else if (val == (*tree)->data)
+    {
+        return *tree;
+    }
 }
 
 void printPreorder(Node* tree) {
@@ -49,24 +85,27 @@ void print_levelorder(Node *tree) {
         print_levelorder(tree->left);
         print_levelorder(tree->right);
     }
-    
-    
 }
 
 int main(void) {
-    Node* root = createNode(1);
-    Node* node2 = createNode(2);
-    Node* node3 = createNode(3);
-    root->left = node2;
-    root->right = node3;
-    node2->left = createNode(4);
-    node2->right = createNode(5);
-    node3->right = createNode(7);
+    Node* root;
+    root = NULL;
+    insertNode(&root, 1);
+    insertNode(&root, 2);
+    insertNode(&root, 3);
+    // insertNode(&root, 10);
+    // insertNode(&root, 12);
+    insertNode(&root, 5);
     printPreorder(root);
     printf("\n");
     printInorder(root);
     printf("\n");
     printpostorder(root);
+    Node* subtree = searchtree(&root, 3);
+    deltree(subtree);
+    printf("\n");
+    insertNode(&root, 6);
+    printPreorder(root);
 
     return 0;
 }
